39f886ba55845cfb93e64dbeeb8e8cdffe6c123c
David Sarmiento Cervantes <fercho_fer_12@hotmail.com>
2021-01-04 21:41:34 -0600

    configuracion y favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: 'Cincel' || '',
+    title: 'Ask' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
